177065d3203a3fd13b0c28587fd0175286b9b55a
MikeVogt86 <97607191+MikeVogt86@users.noreply.github.com>
2022-06-10 22:00:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector(".menu-kleur");
+const siteBody = document.body;
+
+const home = document.querySelector(".home-btn");
+const red = document.querySelector(".red-btn");
+const orange = document.querySelector(".orange-btn");
+const purple = document.querySelector(".purple-btn");
+const green = document.querySelector(".green-btn");
+
+el=document.getElementById("home-btn")
+el.addEventListener("click",(e)=>{
+    console.log(e)
+})
+
+
+
