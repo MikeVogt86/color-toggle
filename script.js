03fb6789a30a10c26003db4fe590e5aebacb855a
MikeVogt86 <97607191+MikeVogt86@users.noreply.github.com>
2022-06-27 10:27:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const purple = document.querySelector(".purple-btn");
 const green = document.querySelector(".green-btn");
 
 hamburger.addEventListener("click", (e) => {
-  hamburger.classList.toggle("active");
   menu.classList.toggle("active");
 });
 
+menu.addEventListener("click", (e) => {
+});
