5ecba931847c7f02f67020d39e078c460fabd869
MikeVogt86 <97607191+MikeVogt86@users.noreply.github.com>
2022-06-28 12:39:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -45,6 +45,6 @@ hamburger.addEventListener("mouseover", (e) => {
 
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function (event) {
-    console.log(event.target);
+    console.log(event.target.value);
   });
 }
